cef0d954c273fa2123baa3d8a74502529b6e25ff
Diane <hello@dianenguyen.ca>
2022-12-01 12:30:55 -0800

    Adding in question

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
+global current_index
+
+current_index = 0
 #we will append answers to responses
 
 @app.get('/')
@@ -18,3 +21,26 @@ def show_home():
     instructions = survey.instructions
 
     return render_template("survey_start.html", title=title, instructions=instructions)
+
+@app.post('/questions')
+def redirect_to_question():
+    return redirect(f"/questions/{current_index}")
+
+@app.get('/questions/<num>')
+def show_question(num):
+
+    question = survey.questions[int(num)]
+    prompt = question.prompt
+    choices = question.choices
+
+    return render_template("question.html", prompt=prompt, choices=choices)
+
+@app.post('/answer')
+def save_answer():
+    choice = request.form["answer"]
+
+    responses.append(choice)
+
+    current_index += 1
+
+    return redirect(f"/questions/{current_index}")
