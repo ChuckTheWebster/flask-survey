79744444177d098a6f7bd57e7adbed808e6073fa
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-12-01 17:55:01 -0700

    questions render and completion template in progress

--- app.py
+++ app.py
@@ -20,27 +20,60 @@ def show_home():
     title = survey.title
     instructions = survey.instructions
 
+    session["responses"] = []
+    session["question_idx"] = 0
+
+    questions_length = len(survey.questions)
+
+    session["questions_length"] = questions_length
+
     return render_template("survey_start.html", title=title, instructions=instructions)
 
-@app.post('/questions')
+@app.post('/begin')
 def redirect_to_question():
-    return redirect(f"/questions/{current_index}")
+    """"""
+    return redirect(f"/questions/0")
 
-@app.get('/questions/<num>')
+
+@app.get('/questions/<int:num>')
 def show_question(num):
+    """"""
+    #{session['question_idx']}
+    #session['question_idx']
 
-    question = survey.questions[int(num)]
+    question = survey.questions[num]
+
     prompt = question.prompt
     choices = question.choices
 
     return render_template("question.html", prompt=prompt, choices=choices)
 
+
+
+
 @app.post('/answer')
 def save_answer():
+    """"""
     choice = request.form["answer"]
 
-    responses.append(choice)
+    session["responses"].append(choice)
+    print(session["responses"])
 
-    current_index += 1
+    if len(session["responses"]) == session["questions_length"]:
+        return redirect("/completion")
 
-    return redirect(f"/questions/{current_index}")
+    else:
+
+
+        session["question_idx"]+=1
+
+        next_idx = session["question_idx"]
+
+        return redirect(f"/questions/{next_idx}")
+
+@app.get('/completion')
+def thank_you():
+
+    questions = survey.questions
+
+    return render_template("completion.html", questions=questions, )
