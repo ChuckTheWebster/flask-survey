1e823c0924605893c002e6fb48d2fef48ce62e90
Diane <hello@dianenguyen.ca>
2022-12-01 17:54:46 -0800

    Add doc strings, render completion when user has answered all questions

--- app.py
+++ app.py
@@ -8,72 +8,61 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-responses = []
-global current_index
-
-current_index = 0
-#we will append answers to responses
 
 @app.get('/')
 def show_home():
+    """Show start survey and initiate session"""
 
     title = survey.title
     instructions = survey.instructions
 
     session["responses"] = []
     session["question_idx"] = 0
-
-    questions_length = len(survey.questions)
-
-    session["questions_length"] = questions_length
+    session["questions_length"] = len(survey.questions)
 
     return render_template("survey_start.html", title=title, instructions=instructions)
 
+
 @app.post('/begin')
 def redirect_to_question():
-    """"""
+    """Redirects to first question"""
     return redirect(f"/questions/0")
 
 
 @app.get('/questions/<int:num>')
 def show_question(num):
-    """"""
-    #{session['question_idx']}
-    #session['question_idx']
-
+    """Shows question to answer"""
     question = survey.questions[num]
-
     prompt = question.prompt
     choices = question.choices
 
     return render_template("question.html", prompt=prompt, choices=choices)
 
 
-
-
 @app.post('/answer')
 def save_answer():
-    """"""
+    """Saves answer to session. If reached end of survey, redirect to Thank You page else, redirect to next question"""
     choice = request.form["answer"]
 
-    session["responses"].append(choice)
-    print(session["responses"])
+    responses = session["responses"]
 
-    if len(session["responses"]) == session["questions_length"]:
+    responses.append(choice)
+
+    session["responses"] = responses
+
+    if len(session["responses"]) >= session["questions_length"]:
         return redirect("/completion")
 
     else:
-
-
         session["question_idx"]+=1
-
         next_idx = session["question_idx"]
 
         return redirect(f"/questions/{next_idx}")
 
+
 @app.get('/completion')
 def thank_you():
-
+    """Displays thank you page and all questions and answers"""
     questions = survey.questions
 
-    return render_template("completion.html", questions=questions, )
+    return render_template("completion.html", questions=questions)
